ee60d8e4f18430f6ef85b3c7ca168199ce9cb9b4
向松涛 <280304286@163.com>
2016-07-13 22:29:00 +0800

    debug

--- public/web/config.js
+++ public/web/config.js
@@ -8,7 +8,6 @@ var CONFIG = {
     // url: "http://172.16.6.55:8080",
     //我的信息_id
     MY_INFO_ID: '576b95155fce2dfd3874e738',
-    // MY_INFO_ID: '578251be1cd6c37a04a4d50b',
     //我对对评论进行回复的信息
     MY: '我',
     EMAIL: '280304286@163.com'
