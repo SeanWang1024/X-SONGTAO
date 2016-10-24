ede03b2fe91ec53857b12d46c6ae2a30b9fbd8f5
向松涛 <280304286@163.com>
2016-10-24 23:42:12 +0800

    MP3支持

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.use(express.static(path.join(__dirname, 'public'), {
 	etag: false, //资源标记
 	maxAge: 0,//30 days 后过期, 单位ms
 	setHeaders: function (res, path, state) {
-		if(/\.(js|css|png|gif|jpg|jpeg|ico)$/.test(path)){
+		if(/\.(js|css|png|gif|jpg|jpeg|ico|mp3)$/.test(path)){
 			// 未来的一个过期时间
 			res.set('Expires', new Date(Date.now() + 2592000*1000).toGMTString())
 			res.set('Cache-Control', 'public, max-age=2592000')
